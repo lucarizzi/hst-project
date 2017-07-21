405416b08579f389b2d1b22f892edc2c7007b940
Luca Rizzi <lrizzi@keck.hawaii.edu>
2017-07-21 09:49:09 -1000

    added the download.js file loaded by the download function

---
+++ spatial-selection/download.js
@@ -0,0 +1,27 @@
+var filetext = 'V-I,I\n';
+var data = s2.data;
+for (i=0; i < data['x'].length; i++) {
+    var currRow = [data['x'][i].toString(),
+                   data['y'][i].toString().concat("\n")];
+
+    var joined = currRow.join();
+    filetext = filetext.concat(joined);
+}
+
+var filename = 'data_result.csv';
+var blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' });
+
+//addresses IE
+if (navigator.msSaveBlob) {
+    navigator.msSaveBlob(blob, filename);
+}
+
+else {
+    var link = document.createElement("a");
+    link = document.createElement('a')
+    link.href = URL.createObjectURL(blob);
+    link.download = filename
+    link.target = "_blank";
+    link.style.visibility = 'hidden';
+    link.dispatchEvent(new MouseEvent('click'))
+}
